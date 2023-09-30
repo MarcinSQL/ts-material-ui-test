228cfc108992a12972bc92e3359e00f66906c1be
Marcin Wasilewski <marcin.wasilewski512@gmail.com>
2023-09-30 19:33:53 +0200

    Created ToolBar with main icon and description

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
 import React from "react";
-import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@mui/material';
-import PhotoCamera from '@mui/icons-material';
+import {
+  Typography,
+  AppBar,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  CssBaseline,
+  Grid,
+  Toolbar,
+  Container,
+} from "@mui/material";
+import { PhotoCamera } from "@mui/icons-material";
 
 function App() {
   return (
-    <div>
-      <Typography variant="h1">Hello world!</Typography>
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <PhotoCamera />
+          <Typography variant="h6"> Photo Album</Typography>
+        </Toolbar>
+      </AppBar>
+    </React.Fragment>
   );
 }
 
