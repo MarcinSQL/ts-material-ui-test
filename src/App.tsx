9db34a8c566a3d2a3346f7c21c469c5feae590b6
Marcin Wasilewski <marcin.wasilewski512@gmail.com>
2023-09-30 19:29:59 +0200

    Added Material UI icons to project with some components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import React from "react";
+import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@mui/material';
+import PhotoCamera from '@mui/icons-material';
+
 function App() {
   return (
     <div>
-      <h1>Hello world!</h1>
+      <Typography variant="h1">Hello world!</Typography>
     </div>
   );
 }
