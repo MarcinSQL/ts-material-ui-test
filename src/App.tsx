a63d3652e87414d9afa5b227c6ebf7b053c702ea
Marcin Wasilewski <marcin.wasilewski512@gmail.com>
2023-09-30 19:42:56 +0200

    Added some welcome speach

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,30 @@ function App() {
           <Typography variant="h6"> Photo Album</Typography>
         </Toolbar>
       </AppBar>
+      <main>
+        <div>
+          <Container maxWidth="sm">
+            <Typography
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              Photo Album
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              Hello everyone This is a photo album and I'm trying to make this
+              sentence as long as possible so we can see how does it look like
+              on the screen.
+            </Typography>
+          </Container>
+        </div>
+      </main>
     </React.Fragment>
   );
 }
